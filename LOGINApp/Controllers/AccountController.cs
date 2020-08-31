b3670a22467fa4aa1d36871a85e2924fbd95eeed
Zuaibk <zuaibkhan34@gmail.com>
2020-08-31 19:43:45 +0100

    MY very first commit

---
+++ LOGINApp/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LOGINApp.Models;
+
+namespace LOGINApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(RegisterModel model)
+        {
+            using (var context = new OfficeEntities())
+            {
+
+                bool isValid = context.Register.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+            
+            if ()
+            }
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(Register model)
+        {
+
+            using (var context = new OfficeEntities())
+            {
+                context.Register.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ LOGINApp/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LOGINApp.Models
+{
+    public class RegisterModel
+    {
+        public int id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
